51bcb65e5d4a62dbd875285617f75e18a1071bdf
krishh16 <krishh16@yahoo.com>
2024-03-04 21:01:04 +0530

    added the backend for creating users

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,9 +1,9 @@
 import { Hono } from 'hono'
-
+import { usersRouter } from './routes/users'
+import { blogRouter } from './routes/blog'
 const app = new Hono()
 
-app.get('/', (c) => {
-  return c.text('Hello Hono!')
-})
+app.route('/api/v1/user', usersRouter)
+app.route('/api/v1/blog', blogRouter)
 
 export default app
---
+++ backend/src/routes/users.ts
@@ -0,0 +1,67 @@
+import { Hono } from "hono";
+import { PrismaClient } from "@prisma/client/edge";
+import { withAccelerate } from "@prisma/extension-accelerate";
+import { sign } from "hono/jwt";
+export const usersRouter = new Hono<{
+    Bindings: {
+        DATABASE_URL: string;
+        JWT_SECRET: string;
+    }
+}>();
+
+usersRouter.post('/signup', async c => {
+    const prisma = new PrismaClient({
+        datasourceUrl: c.env.DATABASE_URL
+    }).$extends(withAccelerate())
+
+    const body = await c.req.json();
+    try{
+    const user = await prisma.user.create({
+        data: {
+            username: body.username,
+            password: body.password,
+            name: body.name,
+            email: body.email
+        }
+    })
+
+    const token = await sign(user.id, c.env.JWT_SECRET)
+    return c.json({token})
+
+}
+    catch (err){
+        c.status(403)
+        return c.text('UAE')
+    }
+})
+
+usersRouter.post('/signin', async c => {
+    const prisma = new PrismaClient({
+        datasourceUrl: c.env.DATABASE_URL
+    }).$extends(withAccelerate())
+
+    const body = await c.req.json();
+    try{
+    const user = await prisma.user.findFirst({
+        where: {
+            password: body.password,
+            email: body.email
+        }
+    })
+    if(!user) {
+        c.status(403)
+        return c.json({
+            message: "incorrect creds"
+        })
+    }
+    const  token = await sign({id: user.id}, c.env.JWT_SECRET)
+    return c.json({
+        token,})
+    
+
+}
+    catch (err){
+        c.status(403)
+        return c.text('UAE')
+    }
+})
